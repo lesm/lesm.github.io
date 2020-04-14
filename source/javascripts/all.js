dd5f65a36d9ed4d14b6a9ce89cd69afa47c81930
Luis Silva <silmar.dll@gmail.com>
2020-04-13 22:21:20 -0500

    Add fortawesome-free

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,6 @@
 import "../../node_modules/jquery/dist/jquery";
 import "../../node_modules/bootstrap/dist/js/bootstrap";
+import "../../node_modules/@fortawesome/fontawesome-free/js/all";
 import "../stylesheets/all.css.scss";
 
 
